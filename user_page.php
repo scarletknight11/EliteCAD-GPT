af0e54612d368d58f112725e2a191fd286ce577f
Pierce <58158428+PiercingMotion@users.noreply.github.com>
2025-07-18 10:55:00 -0400

    Add files via upload
    XAMPP is needed, as well as MYSQL + PHPADMIN

---
+++ user_page.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['email'])) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body style="background: #fff;">
+    
+    <div class="box">
+    <h1>Welcome, <span><?=$_SESSION['name']; ?></span></h1>
+    <p>This is the <span>user</span> page</p>
+    <button onclick="window.location.href='logout.php'">Logout</button>
+
+    </div>
+
+
+</body>
+</html>
